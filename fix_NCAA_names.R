031bac9f98491f366841f3df17990f3a98c37a90
dpwynne <dpwynne@gmail.com>
2021-10-07 20:31:23 -0700

    fix more naming shenanigans

--- fix_NCAA_names.R
+++ fix_NCAA_names.R
@@ -6,11 +6,15 @@ fix_NCAA_names <- function(x){
             x == "N.C. AT" ~ "N.C. A&T",
             str_detect(x, "Alabama AM") ~ "Alabama A&M",
             str_detect(x, "Texas AM") ~ "Texas A&M",
+            str_detect(x, "Florida AM") ~ "Florida A&M",
             x == "LMU (CA) (CA)" | x == "LMU" ~ "LMU (CA)",
             x == "Binghamtonmton" ~ "Binghamton",
             x == "Saint Marys" | x == "St. Marys" ~ "Saint Mary's (CA)",
             x == "Gardner" ~ "Gardner-Webb",
             x == "St. Johns" ~ "St. John's (NY)",
             x == "William Mary" ~ "William & Mary",
+            x == "Bethune" ~ "Bethune-Cookman",
+            x == "Saint Francis" ~ "Saint Francis (PA)",
+            x == "Saint Peters" ~ "Saint Peter's",
             TRUE ~ x)
 }
