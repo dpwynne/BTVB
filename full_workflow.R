04645430dbd05392a777bf609db45df511971622
dpwynne <dpwynne@gmail.com>
2021-10-25 20:37:42 -0700

    add box score scraping

--- full_workflow.R
+++ full_workflow.R
@@ -518,7 +518,7 @@ serve_df2 <- serve_df %>% left_join(NCAA_teams %>% mutate(Name = str_replace(Nam
 
 #### Only if you want to save the ratings for future predictions/evaluations ####
 #library(lubridate)
-#sunday_date <- "2021-10-10"
+#sunday_date <- "2021-10-24"
 #new_ratings_df <- paste0("ratings_", sunday_date, ".csv")
 #write_csv(serve_df2, new_ratings_df)
 
@@ -611,7 +611,7 @@ get_team_future_schedule <- function(team_name, team_id, sport, year){
 
 all_games <- map2(NCAA_teams$Name, NCAA_teams$ID, get_team_future_schedule, sport = "Women's Volleyball", year = 2022)
 
-prediction_date <- "2021-10-17"
+prediction_date <- "2021-10-24"
 games_week <- all_games %>% bind_rows() %>% filter(Date >= (ymd(prediction_date) + ddays(1)), Date < (ymd(prediction_date) + ddays(8)))
 ## Need to add 1-8 days because it counts duration from 12 midnight on the prediction date
 
@@ -643,7 +643,7 @@ games_predictions <- games_week %>% mutate(
                     )  # there's probably a better way to do this but it works for now
   )
 
-games_predictions <- games_predictions[!duplicated(games_predictions$pred_id),] %>% select(-pred_id)
+games_predictions <- games_predictions[!duplicated(games_predictions$pred_id),] #%>% select(-pred_id)
 
 ###### Step 13: Predict the sideout rates for each team ######
 
@@ -721,3 +721,164 @@ games_predictions_final <- games_predictions3 %>% transmute(
 ## You can save the predictions to a csv file if you want
 #predictions_file <- paste0("predictions_", prediction_date, ".csv")
 #write_csv(games_predictions_final, predictions_file)
+
+
+###### Step 15: Download boxscore data and see how good your predictions were
+
+vb_boxscore <- function(box_score_url){
+  game <- vector("list")
+  game_html <- read_html(box_score_url)
+  
+  # Note: for some reason the NCAA occasionally lists box score links but they're broken
+  # This will also work if you put in a valid game address on NCAA website but it's for a future game
+  if(html_text(game_html) == "Game not found"){
+    game <- list(info = NULL,
+                 player_stats = NULL
+    )
+    return(game)
+  }
+  
+  game_date <- (game_html %>% html_nodes("table:nth-child(1)") %>% html_table())[[1]][1,2] %>% 
+    lubridate::parse_date_time(orders = c("mdYHMp", "mdY"))
+  if(length(game_html %>% html_nodes(".errors")) > 0){
+    game_teams_scores <- (game_html %>% html_nodes("table:nth-child(6)") %>% html_table())[[1]]
+  } else{
+    game_teams_scores <- (game_html %>% html_nodes("table:nth-child(5)") %>% html_table())[[1]]
+  }
+  # if there is an error message about the boxscore, then the scores are in the 6th table instead of the 5th
+  
+  ## Remove the team records
+  game_away <- game_teams_scores[2, 1] %>% str_remove(" \\([0-9]+-[0-9]+.*\\)")
+  game_home <- game_teams_scores[3, 1] %>% str_remove(" \\([0-9]+-[0-9]+.*\\)")
+  
+  game_away_sets <- game_teams_scores[2, ncol(game_teams_scores)] %>% as.numeric()
+  game_home_sets <- game_teams_scores[3, ncol(game_teams_scores)] %>% as.numeric()
+  game_away_total_points <- game_teams_scores[2, 2:(ncol(game_teams_scores) - 1)] %>% as.numeric() %>% sum()
+  game_home_total_points <- game_teams_scores[3, 2:(ncol(game_teams_scores) - 1)] %>% as.numeric() %>% sum()
+  
+  if(length(game_html %>% html_nodes(".errors")) > 0){
+    # if there are known errors then we shouldn't scrape the known-error player data
+    game_players_away_stats <- tibble(
+      Player = NA_character_,
+      Pos = NA_character_,
+      S = NA_real_,
+      MS = NA_character_,
+      Kills = NA_real_,
+      Errors = NA_real_,
+      `Total Attacks` = NA_real_,
+      `Hit Pct` = NA_real_,
+      Assists = NA_real_,
+      Aces = NA_real_,
+      SErr = NA_real_,
+      Digs = NA_real_,
+      RErr = NA_real_,
+      `Block Solos` = NA_real_,
+      `Block Assists` = NA_real_,
+      BErr = NA_real_,
+      PTS = NA_real_,	
+      BHE = NA_real_,
+      team = NA_character_,
+      opponent = NA_character_
+    )
+    game_players_home_stats <- game_players_away_stats    
+  } else {
+    game_players_away <- (game_html %>% html_nodes("table:nth-child(11)") %>% html_table())[[1]]
+    game_players_home <- (game_html %>% html_nodes("table:nth-child(13)") %>% html_table())[[1]]
+    
+    game_players_away_stats <- suppressMessages(game_players_away %>% clean_vb_box_score()) %>%
+      mutate(opponent = game_players_home[1,1])
+    game_players_home_stats <- suppressMessages(game_players_home %>% clean_vb_box_score()) %>%
+      mutate(opponent = game_players_away[1,1])
+  }
+  
+  game$info <- data.frame(game_id = str_remove(box_score_url, "https://stats.ncaa.org/game/box_score/"),
+                          date = game_date,
+                          away = game_away,
+                          home = game_home,
+                          away_sets = game_away_sets,
+                          home_sets = game_home_sets,
+                          away_points = game_away_total_points,
+                          home_points = game_home_total_points,
+                          away_pointpct = game_away_total_points/(game_away_total_points + game_home_total_points),
+                          home_pointpct = game_home_total_points/(game_away_total_points + game_home_total_points))
+  game$player_stats <- bind_rows(game_players_away_stats, game_players_home_stats) %>% 
+    mutate(date = game_date,
+           game_id = game$info$game_id)
+  
+  return(game)
+}
+
+# This should clean the box score
+clean_vb_box_score <- function(box_score_table){
+  players_stats <- box_score_table[3:(nrow(box_score_table) - 2),]
+  names(players_stats) <- box_score_table[2,]
+  players_stats$team <- box_score_table[1,1]
+  players_stats <- players_stats %>%
+    mutate(S = as.numeric(str_remove(S, "/")),  # I think the forward slash indicates some kind of season high? 
+           Kills = as.numeric(str_remove(Kills, "/")),
+           Errors = as.numeric(str_remove(Errors, "/")),
+           `Total Attacks` = as.numeric(str_remove(`Total Attacks`, "/")),
+           `Hit Pct` = as.numeric(str_remove(`Hit Pct`, "/")),
+           Assists = as.numeric(str_remove(Assists, "/")),
+           Aces = as.numeric(str_remove(Aces, "/")),
+           SErr = as.numeric(str_remove(SErr, "/")),
+           Digs = as.numeric(str_remove(Digs, "/")),
+           RErr = as.numeric(str_remove(RErr, "/")),
+           `Block Solos` = as.numeric(str_remove(`Block Solos`, "/")),
+           `Block Assists` = as.numeric(str_remove(`Block Assists`, "/")),
+           BErr = as.numeric(str_remove(BErr, "/")),
+           PTS = as.numeric(str_remove(PTS, "/")),
+           BHE = as.numeric(str_remove(BHE, "/"))
+    ) %>%
+    replace_na(list(Kills = 0,
+                    Errors = 0,
+                    `Total Attacks` = 0,
+                    Assists = 0,
+                    Aces = 0,
+                    SErr = 0,
+                    Digs = 0,
+                    RErr = 0,
+                    `Block Solos` = 0,
+                    `Block Assists` = 0,
+                    BErr = 0,
+                    PTS = 0,
+                    BHE = 0))
+  
+  return(players_stats)
+}
+
+
+NCAA_box_url <- NCAA_game_pbp$box_score[which(!is.na(NCAA_game_pbp$box_score))]
+
+all_NCAA_box <- suppressWarnings(map(NCAA_box_url, vb_boxscore))
+
+NCAA_box_df <- (all_NCAA_box %>% transpose())$info %>% bind_rows()
+
+a4 <- NCAA_box_df %>% left_join((NCAA_game_pbp %>% mutate(
+  home = str_replace(home, "\\\\u0026", "&"),
+  away = str_replace(away, "\\\\u0026", "&"),
+  location = str_replace(location, "\\\\u0026", "&")
+)) %>% select(game_id, location, box_score, play_by_play), by = c("game_id"))
+
+a4_winner <- a4 %>% mutate(winner = if_else(away_sets > home_sets, away, home),
+                           sets = away_sets + home_sets)
+
+# Find the missing games that now have play-by-play
+#a4_missing <- a4_winner %>% filter(!(game_id %in% unique(full_pbp$ncaa_match_id)), !is.na(play_by_play))
+
+#new_NCAA_pbp <- map(a4_missing$play_by_play, vb_play_by_play)
+
+new_NCAA_pbp_full <- new_NCAA_pbp %>% bind_rows() %>% fill(away_score, home_score, .direction= "down") %>%
+  filter(!str_detect(player_name, "Set end"))
+new_NCAA_pbp_full <- new_NCAA_pbp_full %>% mutate(id = seq(1, nrow(new_NCAA_pbp_full))) %>%
+  left_join(NCAA_game_pbp %>% select(game_id, location, box_score, play_by_play), by = c("ncaa_match_id" = "game_id")) %>%
+  mutate(ncaa_match_id = as.numeric(ncaa_match_id))
+
+full_pbp2 <- bind_rows(full_pbp, new_NCAA_pbp_full)
+full_pbp2 <- full_pbp2 %>% mutate(id = seq(1, nrow(full_pbp2)))
+
+all_box_file <- paste0("C:/Users/dpwyn/Documents/Volleyball Analytics/BTVB/box_info_",sunday_date, ".csv")
+
+sets_all <- bind_rows(old_box, a4_winner %>% mutate(game_id = as.numeric(game_id))) %>% mutate(date = as.Date(date))
+
+write_csv(sets_all, all_box_file)
